0ad2dd850a4f2874fac91fc90ea5016bbedda712
Ayodele Fagbami <afagbami@gmail.com>
2023-08-16 12:51:57 +0100

    rendered list items dynamically

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,15 +1,14 @@
-
 function ListGroup() {
+  const items = ["Lagos", "Abuja", "Kano", "Ibadan", "Port Harcourt"];
+
   return (
     <>
-        <h1>List</h1>
-        <ul className="list-group">
-          <li className="list-group-item">An item</li>
-          <li className="list-group-item">A second item</li>
-          <li className="list-group-item">A third item</li>
-          <li className="list-group-item">A fourth item</li>
-          <li className="list-group-item">And a fifth one</li>
-        </ul>
+      <h1>List</h1>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li>{item}</li>
+        ))}
+      </ul>
     </>
   );
 }
