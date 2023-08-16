b18e4d3fc723ce06ee5fe076102e41d472044877
Ayodele Fagbami <afagbami@gmail.com>
2023-08-16 12:37:22 +0100

    filled message component

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,7 +1,10 @@
 
 function Message() {
     // JSX: JavaScript XML
-    return <h1>Hello World</h1>;
+    const name = 'Ayodele';
+    if (name)
+        return <h1>Hello {name}</h1>;
+    return <h1>Hello The Man With No Name</h1>;
 }
 
 export default Message;
